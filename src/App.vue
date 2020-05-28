2d77927e63fe51488f3dd3b9e2f1db0dbc2243e6
Francisco Hodge <hello@franciscohodge.com>
2020-05-28 17:12:06 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <input
+      :value="input"
+      class="input"
+      @click="isKeyboardOpen = true"
+      @input="onInputChange"
+      placeholder="Tap on the virtual keyboard to start"
+    >
+    <SimpleKeyboard
+      v-if="isKeyboardOpen"
+      @onChange="onChange"
+      @onKeyPress="onKeyPress"
+      :input="input"
+    />
+  </div>
+</template>
+
+<script>
+import SimpleKeyboard from "./SimpleKeyboard";
+import "./App.css";
+
+export default {
+  name: "App",
+  components: {
+    SimpleKeyboard
+  },
+  data: () => ({
+    input: "",
+    isKeyboardOpen: false
+  }),
+  methods: {
+    onChange(input) {
+      this.input = input;
+    },
+    onKeyPress(button) {
+      if (button === "{enter}") {
+        this.isKeyboardOpen = false;
+      }
+    },
+    onInputChange(input) {
+      this.input = input.target.value;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
